37a8eb41c4c68735a2cef038653cc4eb924c16a1
Hawk87 <tidus.rm@libero.it>
2019-03-04 20:11:01 +0100

    Introdotto mapper

--- src/main/java/it/comuni/comuniitaliani/controller/ComuneController.java
+++ src/main/java/it/comuni/comuniitaliani/controller/ComuneController.java
@@ -1,7 +1,9 @@
 package it.comuni.comuniitaliani.controller;
 
+import it.comuni.comuniitaliani.mapper.Mapper;
 import it.comuni.comuniitaliani.model.Comune;
 import it.comuni.comuniitaliani.repository.ComuneRepository;
+import it.comuni.comuniitaliani.vm.ComuneVM;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
@@ -21,40 +23,56 @@ import reactor.core.publisher.Mono;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/comuni")
 public class ComuneController {
 
     private final ComuneRepository comuneRepository;
+    private final Mapper<ComuneVM, Comune> comuneMapper;
 
-    public ComuneController(ComuneRepository comuneRepository) {this.comuneRepository = comuneRepository;}
+    public ComuneController(ComuneRepository comuneRepository, Mapper<ComuneVM, Comune> comuneMapper) {
+        this.comuneRepository = comuneRepository;
+        this.comuneMapper = comuneMapper;
+    }
 
     @GetMapping("")
-    public Flux<Comune> getAll(@RequestParam String orderBy, @RequestParam Direction type) {
-        return comuneRepository.findAll(Sort.by(type, orderBy));
+    public Flux<ComuneVM> getAll(@RequestParam String orderBy, @RequestParam Direction type) {
+        return comuneRepository
+                .findAll(Sort.by(type, orderBy))
+                .map(comuneMapper::dto2vm);
     }
 
     @GetMapping("/{codice}")
-    public Mono<ResponseEntity<Comune>> get(@PathVariable("codice") Integer codice) {
+    public Mono<ResponseEntity<ComuneVM>> get(@PathVariable("codice") Integer codice) {
         return comuneRepository.findById(codice)
+                .map(comuneMapper::dto2vm)
                 .map(ResponseEntity::ok)
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
 
     @PostMapping("")
-    public Mono<Comune> add(@Valid @RequestBody Comune comune) {
-        return comuneRepository.save(comune);
+    public Mono<ComuneVM> add(@Valid @RequestBody ComuneVM vm) {
+        final Comune comune = comuneMapper.vm2dto(vm);
+        return comuneRepository
+                .save(comune)
+                .map(comuneMapper::dto2vm);
     }
 
     @PostMapping("/all")
-    public Flux<Comune> addAll(@Valid @RequestBody List<Comune> comuni) {
-        return comuneRepository.saveAll(comuni);
+    public Flux<ComuneVM> addAll(@Valid @RequestBody List<ComuneVM> comuni) {
+        return comuneRepository
+                .saveAll(comuni.stream()
+                        .map(comuneMapper::vm2dto)
+                        .collect(Collectors.toList()))
+                .map(comuneMapper::dto2vm);
     }
 
     @PutMapping("/{codice}")
-    public Mono<ResponseEntity<Comune>> update(@PathVariable("codice") Integer codice,
-                                               @Valid @RequestBody Comune comune) {
+    public Mono<ResponseEntity<ComuneVM>> update(@PathVariable("codice") Integer codice,
+                                                 @Valid @RequestBody ComuneVM vm) {
+        final Comune comune = comuneMapper.vm2dto(vm);
         return comuneRepository.findById(codice)
                 .flatMap(existingComune -> {
                     existingComune.setNome(comune.getNome());
@@ -67,6 +85,7 @@ public class ComuneController {
                     existingComune.setPopolazione(comune.getPopolazione());
                     return comuneRepository.save(existingComune);
                 })
+                .map(comuneMapper::dto2vm)
                 .map(updatedComune -> new ResponseEntity<>(updatedComune, HttpStatus.OK))
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
@@ -83,7 +102,9 @@ public class ComuneController {
 
     // Sent to the client as Server Sent Events
     @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<Comune> streamAll() {
-        return comuneRepository.findAll();
+    public Flux<ComuneVM> streamAll() {
+        return comuneRepository
+                .findAll()
+                .map(comuneMapper::dto2vm);
     }
 }
